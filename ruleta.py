d52b14a45e5bfee42653954151e94132aadc408b
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-05-09 14:18:03 -0500

    juego de la ruleta rusa

---
+++ ruleta.py
@@ -0,0 +1,14 @@
+import random
+print("SI GANAS, JUEGA EL OTRO JUGADOR...")
+print("SI PIERDES, ME INVITAS UN FLAN")
+
+print()
+print("Este es el resultado...")
+print()
+
+ai=random.randint(0,5)
+if ai==1:
+    print("HAS PERDIDO")
+else:
+    print("HAS GANADO")
+
